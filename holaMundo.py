456004fe339d0d5c981083f6d6a0b5e9dfa1e085
Julian <julian199664@gmail.com>
2021-05-11 12:19:59 -0500

    Modificando un archivo y subienndo el cambio a GITHUB

--- holaMundo.py
+++ holaMundo.py
@@ -10,9 +10,9 @@ var3=int(input("Ingrese el valor de la variable: "))
 if var3>40:
     print("Mayor a 40")
 elif  var3>30 and var3<=40:
-    print("entre 31 y 40") 
-else:    
-    print("menor o igual a 30")   
+    print("entre 31 y 40")
+else:
+    print("menor o igual a 30")
 '''
 
 peso=float(input("Digite el peso de la persona: "))
@@ -27,7 +27,8 @@ elif  IMC>=17.5 and IMC<25.9:
 elif IMC>=26 and IMC<31:
     print("Sobrepeso")
 else:
-    print("Obesidad")  
-  
+    print("Obesidad")
+
 print("Version 2 commit")
 print("Probando ramas")
+print("Subir cambios a GITHUB")
